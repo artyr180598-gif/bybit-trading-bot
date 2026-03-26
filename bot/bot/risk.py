183722cdebebe3a44f296ff49c2415db2bca4674
artyr180598-gif <artyr180598@gmail.com>
2026-03-26 18:12:14 +0100

    Delete bot/bot/risk.py

--- bot/bot/risk.py
+++
@@ -1,6 +0,0 @@
-def calc_position(balance, atr, risk_percent):
-    risk_amount = balance * (risk_percent / 100)
-    stop_distance = atr * 1.5
-
-    qty = risk_amount / stop_distance
-    return round(qty, 6)
