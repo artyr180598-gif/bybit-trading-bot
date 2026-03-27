b422fa8c00fbeac68dde51db4b0cc364233c2cc5
artyr180598-gif <artyr180598@gmail.com>
2026-03-27 08:09:30 +0100

    Delete bot/notifier.py

--- bot/notifier.py
+++
@@ -1,9 +0,0 @@
-import requests
-from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
-
-def send(msg):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    requests.post(url, json={
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": msg
-    }, timeout=10)
